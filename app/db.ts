b74a02564b0e5105bf01be2eaedc5c6ecc1b6b80
CodeWithfarzu <mdfarzan78@gamil.com>
2026-05-16 17:09:52 +0530

    update

--- app/db.ts
+++ app/db.ts
@@ -1,10 +1,11 @@
 import postgres from 'postgres'
 
 const sql = postgres({
-    host:                  'aws-1-ap-south-1.pooler.supabase.com',            
-    port:                  5432,     
-    database:              'postgres',           
-    username:              'postgres.ukxureomutojxzuyftvh',            
-    password:              'Asansol@12farzan',
+    host:                  'process.env.DB_aws-1-ap-south-1.pooler.supabase.com',            
+    port:                   Number(process.env.DB_5432),     
+    database:              'process.env.DB_postgres',           
+    username:              'process.env.DB_postgres.ukxureomutojxzuyftvh',            
+    password:              'process.env.DB_Asansol@12farzan',
+    max:                    1, //limit connections per serverless function
 });
 export default sql;
--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
-  reactStrictMode:false,
-  images:{
-    domains:['p.w3layouts.com']
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'p.w3layouts.com',
+      }
+    ]
   }
 };
 
