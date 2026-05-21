e7ed55847eb76816f8bbc90810f531140d707e5b
CodeWithfarzu <mdfarzan78@gamil.com>
2026-05-21 19:55:07 +0530

    fix: use single DATABASE_URL connectioin string

--- app/db.ts
+++ app/db.ts
@@ -1,14 +1,9 @@
 import postgres from 'postgres'
 
-const sql = postgres({
-    host: process.env.DB_HOST!,
-    port: Number(process.env.DB_PORT),
-    database: process.env.DB_NAME!,
-    username: process.env.DB_USER!,
-    password: process.env.DB_PASSWORD!,
-    // max: 1,
+const sql = postgres(process.env.DATABASE_URL!, {
+    max: 1,
     ssl: 'require',
-    prepare: false,   // required for Supabase transaction pooler
+    prepare: false,
 });
 
 export default sql;
